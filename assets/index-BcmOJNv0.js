e5b2f1a1aec8387ad34aa7e5d123f103aa666ff3
Mustafa Talha ŞEN <mustalha06@gmail.com>
2025-12-18 15:48:23 +0300

    Add game files for GitHub Pages deployment

---
+++ assets/index-BcmOJNv0.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-Bk7Ra5NS.js","./index-8u52xgSg.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-DrFUXjn3.js";import{r as p}from"./index-8u52xgSg.js";const r=p("App",{web:()=>e(()=>import("./web-Bk7Ra5NS.js"),__vite__mapDeps([0,1]),import.meta.url).then(e=>new e.AppWeb)});export{r as App};
---
+++ assets/index-DihluWxr.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BOKB7WRG.js","assets/index-B_4-ozG1.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-XiszNQDV.js";import{r as p}from"./index-B_4-ozG1.js";const _=p("App",{web:()=>e(()=>import("./web-BOKB7WRG.js"),__vite__mapDeps([0,1])).then(r=>new r.AppWeb)});export{_ as App};
